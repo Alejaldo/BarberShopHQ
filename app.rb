f96b1b17b84955a4545818e96a0e1915f95d8c0b
Alejaldo <statfluct@yahoo.com>
2020-11-27 00:22:12 +0500

    Mamma_Mia

--- app.rb
+++ app.rb
@@ -12,6 +12,22 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
 	erb :index			
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
 end
