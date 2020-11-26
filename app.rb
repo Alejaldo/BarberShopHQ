5a10b7b141ac39d8c4343107e4ed5f52e03b18d0
Alejaldo <statfluct@yahoo.com>
2020-11-27 03:57:41 +0500

    Correction

--- app.rb
+++ app.rb
@@ -30,4 +30,31 @@ post '/visit' do
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 	@color = params[:color]
+
+	hh = {
+		:username => 'Enter correct name',
+		:phone => 'Enter correct phone',
+		:datetime => 'Enter correct date',
+		:time => 'Enter correct time'
+	}
+
+	@error = hh.select { |k,_| params[k] == '' }.values.join(", ")
+	if @error != ''
+		return erb :visit
+	end
+
+	@title = 'Good!'
+	@message = "<h2>Tfck you, you are confirmed epta!</h2>"
+	erb :message
+
+	u = Client.new :name => @username
+	u.save if u.new_record?
+	p = Client.new :phone => @phone
+	p.save if u.new_record?
+	d = Client.new :datestamp => @datetime
+	d.save if u.new_record?
+	b = Client.new :barber => @barber
+	b.save if u.new_record?
+	c = Client.new :color => @color
+	c.save if u.new_record?
 end
